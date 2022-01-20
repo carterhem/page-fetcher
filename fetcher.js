3c5e8341b531f684c33bdcf37ef935d5df639406
Carter Hemington <carter.hemington@gmail.com>
2022-01-20 02:32:07 +0000

    forgot to lint

--- fetcher.js
+++ fetcher.js
@@ -1,20 +1,20 @@
-const fs = require('fs')
-const request = require('request')
+const fs = require('fs');
+const request = require('request');
 
-const args = process.argv.slice(2)
-const url = args[0]
-const filepath = args[1]
+const args = process.argv.slice(2);
+const url = args[0];
+const filepath = args[1];
 
 request(url, (err, response, body) => {
   if (err) {
-    return console.log("Error: ", err.message)
+    return console.log("Error: ", err.message);
   }
 
   fs.writeFile(filepath, body,(err) => {
     if (err) {
-      return console.log("Error: ", err.message)
+      return console.log("Error: ", err.message);
     }
-    console.log(`Downloaded and saved ${body.length} bytes to ${filepath}`)
-    process.exit()
+    console.log(`Downloaded and saved ${body.length} bytes to ${filepath}`);
+    process.exit();
   });
 });
