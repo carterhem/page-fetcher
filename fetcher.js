f3d7817672e0c672db2f1fbe21b45462a98decca
Carter Hemington <carter.hemington@gmail.com>
2022-01-20 02:16:32 +0000

    Pairing with @FemmeSTEMGem

---
+++ fetcher.js
@@ -0,0 +1,20 @@
+const fs = require('fs')
+const request = require('request')
+
+const args = process.argv.slice(2)
+const url = args[0]
+const filepath = args[1]
+
+request(url, (err, response, body) => {
+  if (err) {
+    return console.log("Error: ", err.message)
+  }
+
+  fs.writeFile(filepath, body,(err) => {
+    if (err) {
+      return console.log("Error: ", err.message)
+    }
+    console.log(`Downloaded and saved ${body.length} bytes to ${filepath}`)
+    process.exit()
+  });
+});
